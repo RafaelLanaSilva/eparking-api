e43c8f3efee887b3150d8d501ca71c083366cb9a
RafaelLanaSilva <rafaellanas@gmail.com>
2025-04-15 17:34:19 -0300

    feat: entidade veiculo

---
+++ Eparking/Eparking.Domain/Models/Entities/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using Eparking.Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eparking.Domain.Models.Entities
+{
+    public class Veiculo
+    {
+        public Guid? Id { get; set; }
+        public string? Modelo { get; set; }
+        public string? Placa { get; set; }
+        public string? Cor { get; set; }
+        public TipoVeiculo? TipoVeiculo { get; set; }
+
+        #region Relacionamentos
+
+
+
+        #endregion
+
+    }
+
+}
---
+++ Eparking/Eparking.Domain/Models/Enums/TipoVeiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eparking.Domain.Models.Enums
+{
+    public enum TipoVeiculo
+    {
+        Carro = 1,
+        Moto = 2
+    }
+}
