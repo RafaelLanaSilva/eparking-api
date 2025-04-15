68c2d34e36d075d44c0d98c98379fd5debafd55e
Eletr0n1c <danielmoraessid@gmail.com>
2025-04-15 18:23:07 -0300

    feat: modelagem das entidades

---
+++ Eparking/Eparking.Domain/Models/Entities/Movimentacao.cs
@@ -0,0 +1,25 @@
+﻿namespace Eparking.Domain.Models.Entities
+{
+    public class Movimentacao
+    {
+        #region Propriedades
+
+        public Guid Id { get; set; }
+        public Guid EstaciomentoId { get; set; }
+        public Guid VagaId { get; set; }
+        public Guid VeiculoId { get; set; }
+        public DateTime? HoraEntrada { get; set; }
+        public DateTime? HoraSaida { get; set; }
+        public decimal? ValorCobrado { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+
+        public Vaga? Vaga { get; set; }
+        public Veiculo? Veiculo { get; set; }
+        public ICollection<Estacionamento>? Estacionamentos { get; set; }
+
+        #endregion
+    }
+}
---
+++ Eparking/Eparking.Domain/Models/Entities/Tarifa.cs
@@ -0,0 +1,25 @@
+﻿using Eparking.Domain.Models.Enums;
+
+namespace Eparking.Domain.Models.Entities
+{
+    public class Tarifa
+    {
+        #region Propriedades
+
+        public Guid Id { get; set; }
+        public Guid EstacionamentoId { get; set; }
+        public TipoVeiculo? TipoVeiculo { get; set; }
+        public decimal? ValorHora { get; set; }
+        public decimal? ValorFracao { get; set;} = null;
+        public DateTime? ToleranciaMinutos { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+
+        public Estacionamento? Estacionamento { get; set; }
+
+        #endregion
+
+    }
+}
