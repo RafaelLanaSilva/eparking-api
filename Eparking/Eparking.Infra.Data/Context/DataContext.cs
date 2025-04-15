f8d654e284d2b85cd4d4174a5fae135befd712d0
RafaelLanaSilva <rafaellanas@gmail.com>
2025-04-15 19:10:00 -0300

    feat: banco criado

---
+++ Eparking/Eparking.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Eparking.Infra.Data.Settings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eparking.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(SqlServerSettings.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
