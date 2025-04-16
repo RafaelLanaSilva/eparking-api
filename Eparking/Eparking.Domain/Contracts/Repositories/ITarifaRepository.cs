13caefee0b690aa208f42a41cf1733d05d047759
Eletr0n1c <danielmoraessid@gmail.com>
2025-04-15 22:19:06 -0300

    feat: modelagem dos repositorios

--- Eparking/Eparking.Domain/Contracts/Repositories/ITarifaRepository.cs
+++ Eparking/Eparking.Domain/Contracts/Repositories/ITarifaRepository.cs
@@ -6,7 +6,7 @@ namespace Eparking.Domain.Contracts.Repositories
     public interface ITarifaRepository : IBaseRepository<Tarifa>
     {
         List<Tarifa>? ObterPorEstacionamentoId(Guid estacionamentoId);
-        Tarifa? ObterPorTipoVeiculo(Guid estacionamento, TipoVeiculo tipoVeiculo);
+        Tarifa? ObterPorTipoVeiculo(Guid estacionamentoId, TipoVeiculo tipoVeiculo);
         Tarifa? ObterTarifaPorId(Guid id);
     }
 }
---
+++ Eparking/Eparking.Infra.Data/Repositories/VagaRepository.cs
@@ -0,0 +1,53 @@
+﻿using Eparking.Domain.Contracts.Repositories;
+using Eparking.Domain.Models.Entities;
+using Eparking.Domain.Models.Enums;
+using Eparking.Infra.Data.Context;
+
+namespace Eparking.Infra.Data.Repositories
+{
+    public class VagaRepository : BaseRepository<Vaga>, IVagaRepository
+    {
+        public List<Vaga>? ObterVagasDisponiveis(Guid estacionamentoId)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                    .Set<Vaga>()
+                    .Where(v => v.EstacionamentoId == estacionamentoId && v.Ocupado == false)
+                    .ToList();
+            }
+        }
+
+        public List<Vaga>? ObterVagasOcupadas(Guid estacionamentoId)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                    .Set<Vaga>()
+                    .Where(v => v.EstacionamentoId == estacionamentoId && v.Ocupado == true)
+                    .ToList();
+            }
+        }
+
+        public List<Vaga>? ObterVagasPorTipo(TipoVaga tipoVaga)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                    .Set<Vaga>()
+                    .Where(v => v.TipoVaga == tipoVaga)
+                    .ToList();
+            }
+        }
+
+        public Vaga? ObterPorNúmero(Guid estacionamentoId, int numero)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext
+                    .Set<Vaga>()
+                    .FirstOrDefault(v => v.EstacionamentoId == estacionamentoId && v.Numero == numero);
+            }
+        }
+    }
+}
