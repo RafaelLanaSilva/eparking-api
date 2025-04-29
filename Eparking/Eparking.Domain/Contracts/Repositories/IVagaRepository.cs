aa12a694074657b03838c16f21da77f8ca487529
RafaelLanaSilva <rafaellanas@gmail.com>
2025-04-29 19:18:50 -0300

    fix: vagas repository

--- Eparking/Eparking.Domain/Contracts/Repositories/IVagaRepository.cs
+++ Eparking/Eparking.Domain/Contracts/Repositories/IVagaRepository.cs
@@ -5,6 +5,7 @@ namespace Eparking.Domain.Contracts.Repositories
 {
     public interface IVagaRepository : IBaseRepository<Vaga>
     {
+        void AddRange(IEnumerable<Vaga> vagas);
         Vaga? ObterVagaPorId(Guid id);
         List<Vaga>? ObterVagasDisponiveis(Guid estacionamentoId);
         List<Vaga>? ObterVagasOcupadas(Guid estacionamentoId);
